12d2c8486b83ee031620ee9114fe838735d1d914
AnnaKriuchkova <nura.spirina@yandex.ru>
2022-11-25 16:58:55 +0300

    Sem4 Added

---
+++ Sem4Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double MethodPow(int a, int b)
+{
+    double result = Math.Pow(a,b);
+    return result;
+}
+
+Console.WriteLine("Введите перое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(MethodPow(num, num2));
+
+
---
+++ Sem4Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int GetSumNum(string text)
+{
+    int sum = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        string symbol = text[i].ToString(); // char text[i]
+        // Console.WriteLine(symbol);
+        // Console.WriteLine(Convert.ToInt32(symbol));
+        sum = sum + Convert.ToInt32(symbol);
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+
+if (num >= 0)
+{
+    Console.WriteLine(GetSumNum(str));
+}
+else 
+{
+    Console.WriteLine("Ошибка");
+}
