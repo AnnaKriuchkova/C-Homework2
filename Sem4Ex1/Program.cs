f7838c2378ccd700e4740e9ce97993714e5a5096
AnnaKriuchkova <nura.spirina@yandex.ru>
2022-11-26 11:06:16 +0300

    Changes added

--- Sem4Ex1/Program.cs
+++ Sem4Ex1/Program.cs
@@ -4,18 +4,20 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-double MethodPow(int a, int b)
+int MethodPow(int a, int b)
 {
-    double result = Math.Pow(a,b);
-    return result;
+    int pow = a;
+    for (int i = 1; i < b; i++)
+        {
+            pow = pow*a;
+        }
+        return pow;
 }
-
+    
 Console.WriteLine("Введите перое число");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(MethodPow(num, num2));
-
-
+Console.WriteLine(num+" в "+num2+" степени равно "+(MethodPow(num, num2)));
