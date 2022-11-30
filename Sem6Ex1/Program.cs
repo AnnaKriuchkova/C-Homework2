42645d0f85baec38dd97fb94bdc38246f5e2285d
AnnaKriuchkova <nura.spirina@yandex.ru>
2022-11-30 13:39:17 +0300

    Sem6

---
+++ Sem6Ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите количество чисел");
+int count = Convert.ToInt32(Console.ReadLine());
+int positive = 0;
+
+int [] array = new int [count];
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine("Введите число");
+    int num =Convert.ToInt32(Console.ReadLine());
+
+    array[i]= num;
+    if(array[i] > 0)
+    {
+        positive++;
+    }
+}
+
+Console.WriteLine(String.Join(" ",array));
+Console.WriteLine("Количество чисел больше 0 равно "+positive);
